7fb2fa6a481d5ed594ccfedcf41cc64954b18a84
Daniel Moberg <daniel@moberg.no>
2018-06-09 23:10:14 +0200

    hello2

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.setHeader("Content-Type", "application/json; charset=utf-8");
-	res.send(JSON.parse({msg: "HELLO"}));
+	res.send("HELLO");
 });
 
 module.exports = router;
