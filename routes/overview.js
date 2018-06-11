9858e1c73c581854f273482012e55fef63585c07
Daniel Moberg <daniel@moberg.no>
2018-06-11 15:29:47 +0200

    acao + acah comment

--- routes/overview.js
+++ routes/overview.js
@@ -7,8 +7,8 @@ router.get('/', function(req, res, next) {
     var connection = db
     connection.query('SELECT work.id, users.name, work.workDate, work.workFrom, work.workTo, work.comment FROM work INNER JOIN users ON users.id = work.user', function(err, rows, fields) {
     if (!err) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        // res.header("Access-Control-Allow-Origin", "*");
+        // res.header("Access-Control-Allow-Headers", "X-Requested-With");
         res.set({ 'Content-Type': 'application/json; charset=utf-8' })
         res.setHeader('Content-Type', 'application/json; charset=utf-8')
         res.json(rows)
